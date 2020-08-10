9b234c941f1002114ce3bbb82515c053bbca9e01
Lefti <inserid@gmail.com>
2020-08-10 16:31:12 +0200

    b7

--- src/Lefti.java
+++ src/Lefti.java
@@ -1,11 +1,12 @@
 public class Lefti {
 
 
-    public void count(String age) {
+    public void count(String age , String name) {
 
         //System.out.println("I am " + age + " years old");
         System.out.println("5");
         System.out.println("13");
         System.out.println("115");
+        System.out.println("name");
     }
 }
