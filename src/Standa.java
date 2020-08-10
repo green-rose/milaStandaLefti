280f49155a94a74b2a3ace4891a93f31e466b688
Stanoso <standa.dostal@gmail.com>
2020-08-10 16:00:05 +0200

    c3

--- src/Standa.java
+++ src/Standa.java
@@ -2,6 +2,8 @@ public class Standa {
 
     public void cheer(String teamName) {
         System.out.println(teamName);
+        System.out.println(teamName);
+        System.out.println(teamName);
     }
 
 }
