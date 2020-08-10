5858f1e900fdf7b5ab3a294f50dcad9ea9daba5e
Mila Karous <m.k.rose@seznam.cz>
2020-08-10 15:43:44 +0200

    a3

--- src/Lefti.java
+++ src/Lefti.java
@@ -6,6 +6,7 @@ public class Lefti {
 
     public void count(String age){
 
-        System.out.println("I am " + age + " years old");
+        //System.out.println("I am " + age + " years old");
+        System.out.println("5");
     }
 }
