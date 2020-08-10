d8c453075535ddc0b327000e09092d3e9045f70b
Lefti <inserid@gmail.com>
2020-08-10 16:43:44 +0200

    b8

--- src/Mila.java
+++ src/Mila.java
@@ -5,7 +5,7 @@ public class Mila {
     }
 
     public static void greet(String name){
-        System.out.println("Hello" + name);
+        System.out.println("Hellooooooo" + name +" !!");
     }
 
 }
