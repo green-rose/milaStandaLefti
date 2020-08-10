c0205035fc9fcb55d29860cce98e313b00a63306
Mila Karous <m.k.rose@seznam.cz>
2020-08-10 16:09:31 +0200

    Update Mila.java

--- src/Mila.java
+++ src/Mila.java
@@ -5,9 +5,7 @@ public class Mila {
     }
 
     public static void greet(String name){
-
-        System.out.println(name+" is name.");
-        System.out.println("Hello" + name);
+        System.out.println("MILA");
     }
 
 }
