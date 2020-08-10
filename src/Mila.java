aafe526c99e60e79d894cca9d9e0cf3c6465414f
Stanoso <standa.dostal@gmail.com>
2020-08-10 16:03:45 +0200

    c4

--- src/Mila.java
+++ src/Mila.java
@@ -6,6 +6,8 @@ public class Mila {
 
     public static void greet(String name){
         System.out.println("Hello" + name);
+        System.out.println("Hello" + name);
+        System.out.println("Hello" + name);
     }
 
 }
